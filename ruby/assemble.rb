8ed00ffebff94aa94d7b146b08e984a647bc05f5
Grayson Wright <wright.grayson@gmail.com>
2017-02-14 18:40:27 -0800

    Update ruby scaffold to support custom working dir

--- ruby/assemble.rb
+++ ruby/assemble.rb
@@ -2,11 +2,15 @@ require "json"
 
 class Assemble
   def self.current_run
-    Struct.
-      new(:block_name, :input).
-      new(
-        ENV.fetch("ASSEMBLE_BLOCK_NAME"),
-        JSON.parse(File.read("input.json")),
-      )
+    Run.new(
+      ENV.fetch("ASSEMBLE_BLOCK_NAME"),
+      ENV.fetch("ASSEMBLE_DIR", "/.assemble")
+    )
+  end
+
+  class Run < Struct.new(:block_name, :assemble_dir)
+    def input
+      JSON.parse(File.read(File.join(assemble_dir, "input.json")))
+    end
   end
 end
